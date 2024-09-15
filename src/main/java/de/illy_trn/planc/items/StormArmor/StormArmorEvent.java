463b4062ea00fc242dc715752e1f2f51dbb0c470
illy-dev <illythehase@gmail.com>
2024-09-15 19:49:49 +0200

    renamed everything properly

--- src/main/java/de/illy_trn/planc/items/StormArmor/StormArmorEvent.java
+++ src/main/java/de/illy_trn/planc/items/StormArmor/StormArmorEvent.java
@@ -6,26 +6,15 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 
 public class StormArmorEvent implements Listener {
 
     @EventHandler
     public void onEquip(PlayerArmorChangeEvent e) {
         Player player = e.getPlayer();
-        if (isWearingFullEmeraldArmor(player)) {
-            if (!player.hasPotionEffect(PotionEffectType.HERO_OF_THE_VILLAGE) && !player.hasPotionEffect(PotionEffectType.SPEED)) {
-                player.addPotionEffect(new PotionEffect(PotionEffectType.HERO_OF_THE_VILLAGE, Integer.MAX_VALUE, 0, false, false, true));
-                player.addPotionEffect(new PotionEffect(PotionEffectType.HERO_OF_THE_VILLAGE, Integer.MAX_VALUE, 0, false, false, true));
-            }
-        } else {
-            player.removePotionEffect(PotionEffectType.HERO_OF_THE_VILLAGE);
-            player.removePotionEffect(PotionEffectType.SPEED);
-        }
     }
 
-    private boolean isWearingFullEmeraldArmor(Player player) {
+    private boolean isWearingFullStormArmor(Player player) {
         ItemStack[] armorContents = player.getInventory().getArmorContents();
 
         if (armorContents.length != 4) {
@@ -37,14 +26,14 @@ public class StormArmorEvent implements Listener {
         String stormLeggingsName = "§6Storm Leggings";
         String stormBootsName = "§6Storm Boots";
 
-        return isEmeraldArmorPiece(armorContents[3], stormHelmetName) &&
-               isEmeraldArmorPiece(armorContents[2], stormChestplateName) &&
-               isEmeraldArmorPiece(armorContents[1], stormLeggingsName) &&
-               isEmeraldArmorPiece(armorContents[0], stormBootsName);
+        return isStormArmorPiece(armorContents[3], stormHelmetName) &&
+               isStormArmorPiece(armorContents[2], stormChestplateName) &&
+               isStormArmorPiece(armorContents[1], stormLeggingsName) &&
+               isStormArmorPiece(armorContents[0], stormBootsName);
     }
 
 
-    private boolean isEmeraldArmorPiece(ItemStack item, String expectedName) {
+    private boolean isStormArmorPiece(ItemStack item, String expectedName) {
         if (item == null || !item.hasItemMeta()) {
             return false;
         }
