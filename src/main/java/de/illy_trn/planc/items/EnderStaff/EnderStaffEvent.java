b48b782392071b37fff52316438a2e509d912606
illy-dev <illythehase@gmail.com>
2024-09-16 14:40:35 +0200

    changed ender staff

--- src/main/java/de/illy_trn/planc/items/EnderStaff/EnderStaffEvent.java
+++ src/main/java/de/illy_trn/planc/items/EnderStaff/EnderStaffEvent.java
@@ -1,6 +1,7 @@
 package de.illy_trn.planc.items.EnderStaff;
 
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
@@ -12,17 +13,20 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class EnderStaffEvent implements Listener {
+    int cooldownTime = 8;
+
     @EventHandler
     public void onPlayerClicks(PlayerInteractEvent e) {
         Player player = e.getPlayer();
         Action action = e.getAction();
 
          if (action.equals(Action.RIGHT_CLICK_AIR) || action.equals(Action.RIGHT_CLICK_BLOCK)) {
-             if (player.getItemInHand().getType() == Material.STICK) {
+             if (player.getItemInHand().getType() == Material.WOODEN_SWORD && player.getItemInHand().getItemMeta().getDisplayName().equals(ChatColor.LIGHT_PURPLE + "Enderstaff")) {
                  Block b = player.getTargetBlock(null, 15);
                 Location loc = new Location(b.getWorld(), b.getX(), b.getY(), b.getZ(), player.getLocation().getYaw(), player.getLocation().getPitch());player.teleport(loc);
                 player.teleport(loc);
                 player.playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 1f,1f);
+
 
              }
          }
