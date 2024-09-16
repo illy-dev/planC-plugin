bf01b61c06709138ae1562b64d3b3438549a1d64
TheRedNo <noah.sammiller@gmail.com>
2024-09-16 15:47:23 +0200

    Crystal Diamond Axe is Ready

--- src/main/java/de/illy_trn/planc/items/CrystalDiamond/axe/axeEvent.java
+++ src/main/java/de/illy_trn/planc/items/CrystalDiamond/axe/axeEvent.java
@@ -1,5 +1,6 @@
 package de.illy_trn.planc.items.CrystalDiamond.axe;
 
+import net.kyori.adventure.text.BlockNBTComponent;
 import net.md_5.bungee.api.chat.BaseComponent;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -18,9 +19,23 @@ public class axeEvent implements Listener {
         Player player = event.getPlayer();
         Block breakBlock = event.getBlock();
 
-        if (player.getItemInHand().getType() == Material.NETHERITE_AXE && player.getItemInHand().getItemMeta().getDisplayName().equals(ChatColor.DARK_PURPLE + "Crystal Diamond Axe")) {
+        if (player.getItemInHand().getType() == Material.NETHERITE_AXE && player.getItemInHand().getItemMeta().getDisplayName()
+                .equals(ChatColor.DARK_PURPLE + "Crystal Diamond Axe") && isLog(breakBlock.getType())) {
             Location PosBreakBlock = breakBlock.getLocation();
 
+
+            for (int i = 1; i <= 6; i++) {
+                // Hol die Position des Blocks i Blöcke über dem abgebauten Block
+                Block aboveBlock = PosBreakBlock.clone().add(0, i, 0).getBlock();
+
+                // Wenn der obere Block auch ein Holzstamm ist, baue ihn ab
+                if (isLog(aboveBlock.getType())) {
+                    aboveBlock.breakNaturally();  // Baut den Block auf natürliche Weise ab
+                } else {
+                    // Wenn der Block kein Holzstamm mehr ist, beende die Schleife
+                    break;
+                }
+            }
 
 
 
@@ -29,6 +44,15 @@ public class axeEvent implements Listener {
 
         }
 
+    }
+
+    private boolean isLog(Material material) {
+        // Liste der unterstützten Baumstämme
+        return material == Material.OAK_LOG || material == Material.BIRCH_LOG ||
+                material == Material.SPRUCE_LOG || material == Material.JUNGLE_LOG ||
+                material == Material.ACACIA_LOG || material == Material.DARK_OAK_LOG ||
+                material == Material.MANGROVE_LOG || material == Material.CHERRY_LOG ||
+                material == Material.WARPED_STEM || material == Material.CRIMSON_STEM;
     }
 
 }
