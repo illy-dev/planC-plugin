8a2bd44da5c57c39b3d428ed43389715af0a3e34
illy-dev <illythehase@gmail.com>
2024-09-16 15:13:56 +0200

    Update axe.java

--- src/main/java/de/illy_trn/planc/items/CrystalDiamond/axe/axe.java
+++ src/main/java/de/illy_trn/planc/items/CrystalDiamond/axe/axe.java
@@ -14,7 +14,7 @@ public class axe {
         ItemStack item = new ItemStack(Material.NETHERITE_AXE, 1);
         ItemMeta meta = item.getItemMeta();
         //10 blöcke 8 sekunden
-        List<String> itemLore_axe = Arrays.asList("§7Mine 6 WoodBlocks above the mined block ", "", "§5§lEPIC AXE");
+        List<String> itemLore_axe = Arrays.asList("§7Mine §b6§7 WoodBlocks above the mined block ", "", "§5§lEPIC AXE");
 
         meta.setDisplayName(ChatColor.DARK_PURPLE + "Crystal Diamond Axe");
         meta.setLore(itemLore_axe);
