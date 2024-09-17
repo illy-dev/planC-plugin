3a909c26a93f226a7febe39ec99d0b3bc5317e84
TheRedNo <noah.sammiller@gmail.com>
2024-09-17 14:58:32 +0200

    gefixt

--- src/main/java/de/illy_trn/planc/PlanC.java
+++ src/main/java/de/illy_trn/planc/PlanC.java
@@ -49,7 +49,6 @@ public final class PlanC extends JavaPlugin {
         getCommand("kit").setExecutor(new kit());
         getCommand("tpa").setExecutor(new tpaCommand());
         getCommand("tpaccept").setExecutor(new tpacceptCommand());
-        getCommand("tpadeclinet").setExecutor(new tpaDecline());
         getCommand("home").setExecutor(new home());
         getCommand("home").setTabCompleter(new tabcompleters());
 
